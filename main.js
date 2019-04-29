2ee38db1439672e090b4deacbfb66e8c3888ac6c
rhondamak <rhonda.mak.rm@gmail.com>
2019-04-28 23:06:18 -0400

    fix chiswick bug

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ $("path, circle").mouseleave(function(e) {
   $('#info-box').css('display','none');
 });
 
+$("path, ellipse").mouseleave(function(e) {
+  $('#info-box').css('display','none');
+});
+
 $(document).mousemove(function(e) {
   $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
   $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
