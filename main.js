e6cfa491f193da9628d20b9d81cd24048231afe9
rhondamak <rhonda.mak.rm@gmail.com>
2019-04-29 22:44:22 -0400

    generate copy for each stop

--- main.js
+++ main.js
@@ -1,3 +1,242 @@
+const stopData = { 
+  "BC_Stop_d":{ 
+    "proper_name": "Boston College",
+    "time_saved" : "0m 0s: This is the terminus",
+    "time_stamp" : "0:00",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : "No bus connections",
+    "nearbyTacos": "",
+  },
+
+  "Hynes_Stop_d":{ 
+    "proper_name": "Hynes Convention Center",
+    "time_saved" : "0m 52s",
+    "time_stamp" : "0:52",
+    "nearbyTrainStops" : ["C Line, D Line"],
+    "nearbyBusStops" : ["1, CT1"],
+    "nearbyTacos": "",
+  },
+
+    "Kenmore_Stop_d":{ 
+      "proper_name": "Kenmore",
+      "time_saved" : "1m 1s",
+      "time_stamp" : "1:01",
+      "nearbyTrainStops" : ["C Line, D Line"],
+      "nearbyBusStops" : ["8, 19, 57, 57A, 60, 65"],
+      "nearbyTacos": "",
+  },
+
+  "Blandford_St_Stop_d":{ 
+    "proper_name": "Blandford Street",
+    "time_saved" : "0m 50s",
+    "time_stamp" : "0:50",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["57, 57A"],
+    "nearbyTacos": "",
+  },
+
+  "BU_East_Stop_d":{ 
+    "proper_name": "Boston University East",
+    "time_saved" : "0m 54s",
+    "time_stamp" : "0:54",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["57, 57A"],
+    "nearbyTacos": "",
+  },
+
+  "BU_Central_Stop_d":{ 
+    "proper_name": "Boston University Central",
+    "time_saved" : "0m 49s",
+    "time_stamp" : "0:49",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["57, 57A", "47", "CT2"],
+    "nearbyTacos": "",
+  },
+
+  "BU_West_Stop_d":{ 
+    "proper_name": "Boston University West",
+    "time_saved" : "0m 43s",
+    "time_stamp" : "0:43",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["57, 57A"],
+    "nearbyTacos": "",
+  },
+
+  "Saint_Paul_St_Stop_d":{ 
+    "proper_name": "Saint Paul Street",
+    "time_saved" : "0m 43s",
+    "time_stamp" : "0:43",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["57, 57A"],
+    "nearbyTacos": "",
+  },
+
+  "Pleasant_St_Stop_d":{ 
+    "proper_name": "Pleasant Street",
+    "time_saved" : "0m 48s",
+    "time_stamp" : "0:48",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["57, 57A"],
+    "nearbyTacos": "",
+  },
+
+  "Babcock_St_Stop_d":{
+    "proper_name": "Babcock Street", 
+    "time_saved" : "0m 47s",
+    "time_stamp" : "0:47",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["57, 57A"],
+    "nearbyTacos": "",
+  },
+
+  "Packards_Corner_Stop_d":{ 
+    "proper_name": "Packards Corner",
+    "time_saved" : "0m 56s",
+    "time_stamp" : "0:56",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["57, 57A"],
+    "nearbyTacos": "",
+  },
+
+  "Harvard_Ave_Stop_d":{ 
+    "proper_name": "Harvard Avenue",
+    "time_saved" : "0m 50s",
+    "time_stamp" : "0:50",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["57, 57A", "66"],
+    "nearbyTacos": "",
+  },
+
+  "Griggs_St_Stop_d":{ 
+    "proper_name": "Griggs Street",
+    "time_saved" : "0m 47s",
+    "time_stamp" : "0:47",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : "No bus connections",
+    "nearbyTacos": "",
+  },
+
+  "Allston_St_Stop_d":{ 
+    "proper_name": "Allston Street",
+    "time_saved" : "0m 50s",
+    "time_stamp" : "0:50",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : "No bus connections",
+    "nearbyTacos": "",
+  },
+
+  "Warren_St_Stop_d":{ 
+    "proper_name": "Warren Street",
+    "time_saved" : "0m 40s",
+    "time_stamp" : "0:40",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : "No bus connections",
+    "nearbyTacos": "",
+  },
+
+  "Washington_St_Stop_d":{ 
+    "proper_name": "Washington Street",
+    "time_saved" : "0m 39s",
+    "time_stamp" : "0:39",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : "65",
+    "nearbyTacos": "",
+  },
+
+  "Sutherland_Rd_Stop_d":{ 
+    "proper_name": "Sutherland Road",
+    "time_saved" : "0m 49s",
+    "time_stamp" : "0:49",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : "No bus connections",
+    "nearbyTacos": "",
+  },
+
+  "Chiswick_Rd_Stop_d":{ 
+    "proper_name": "Chiswick Road",
+    "time_saved" : "0m 48s",
+    "time_stamp" : "0:48",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : "No bus connections",
+    "nearbyTacos": "",
+  },
+
+  "Chestnut_Hill_Ave_Stop_d":{ 
+    "proper_name": "Chestnut Hill Avenue",
+    "time_saved" : "0m 40s",
+    "time_stamp" : "0:40",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["51, 86"],
+    "nearbyTacos": "",
+  },
+
+  "South_St_Stop_d":{ 
+    "proper_name": "South Street",
+    "time_saved" : "0m 40s",
+    "time_stamp" : "0:40",
+    "nearbyTrainStops" : "No train connections",
+    "nearbyBusStops" : ["51. 86"],
+    "nearbyTacos": "",
+  },
+
+  "Copley_Stop_d":{ 
+    "proper_name": "Copley",
+    "time_saved" : "0m 54s",
+    "time_stamp" : "0:54",
+    "nearbyTrainStops" : ["C Line, D Line, E Line"],
+    "nearbyBusStops" : ["9, 10, 39, 43, 45, 98, 99"],
+    "nearbyTacos": "",
+  },
+
+  "Arlington_Stop_d":{ 
+    "proper_name": "Arlington",
+    "time_saved" : "0m 54s",
+    "time_stamp" : "0:54",
+    "nearbyTrainStops" : ["C Line, D Line, E Line"],
+    "nearbyBusStops" : ["9, 43, 98, 99"],
+    "nearbyTacos": "",
+  },
+
+  "Boylston_Stop_d":{ 
+    "proper_name": "Boylston",
+    "time_saved" : "0m 50s",
+    "time_stamp" : "0:50",
+    "nearbyTrainStops" : ["C Line, D Line, E Line"],
+    "nearbyBusStops" : ["43, 98, 99"],
+    "nearbyTacos": "",
+  },
+
+  "Park_St_Stop_d":{ 
+    "proper_name": "Park Street",
+    "time_saved" : "0m 0s: This is the terminus",
+    "time_stamp" : "0:00",
+    "nearbyTrainStops" : ["C Line, D Line, E Line"],
+    "nearbyBusStops" : ["15, 39, 43, 55, 98, 99"],
+    "nearbyTacos": "",
+  }
+};
+
+for (var stop in stopData){
+  var currentDiv = document.getElementById(stop.toString());
+  let pname = stopData[stop].proper_name;
+  let nearTrain = stopData[stop].nearbyTrainStops;
+  let nearBus = stopData[stop].nearbyBusStops;
+  let welcomeP = document.createElement("p");
+  let busBlurbP = document.createElement("p");
+  let trainBlurbP = document.createElement("p");
+  let welcomeblurb = "Welcome to " + pname + "!";
+  let busBlurb = "Nearby bus stops: " + nearBus;
+  let trainBlurb = "Nearby train stops: " + nearTrain;
+  let welcomeblurbText = document.createTextNode(welcomeblurb);
+  let busblurbText = document.createTextNode(busBlurb);
+  let trainBlurbText = document.createTextNode(trainBlurb);
+  welcomeP.appendChild(welcomeblurbText);
+  busBlurbP.appendChild(busblurbText);
+  trainBlurbP.appendChild(trainBlurbText);
+  currentDiv.appendChild(welcomeP);
+  currentDiv.appendChild(busBlurbP);
+  currentDiv.appendChild(trainBlurbP);
+}
 $("circle, ellipse").hover(function(e) {
   $('#info-box').css('display','block');
   $('#info-box').html($(this).data('info'));
